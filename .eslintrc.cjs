7b1235711109d4585f91acda3a573e964d84d8f7
hcwhan <hcwhan@gmail.com>
2022-05-27 11:30:06 +0800

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  // 'eslint-config-hcwhan/javascript'    用于 javascript 项目
+  // 'eslint-config-hcwhan/typescript'    用于 typescript 项目
+  // 'eslint-config-hcwhan/vue3'          等价于
+  // 'eslint-config-hcwhan/vue3/lang-ts'  用于 vue3-typescript 项目
+  // 'eslint-config-hcwhan/vue3/lang-js'  用于 vue3-javascript 项目
+
+  extends: ['eslint-config-hcwhan/vue3'],
+}
